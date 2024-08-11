aed42306e8218f3aa65398c8c36fdb50b056fe42
Allen Nagtalon <ant.nagtalon22@gmail.com>
2024-08-11 11:49:58 -0700

    Fix API URL to use 8085 as port

--- frontend/src/api/WishService.js
+++ frontend/src/api/WishService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080/wishes'
+const API_URL = 'http://localhost:8085/wishes'
 
 export async function saveWish (wish) {
   return await axios.post(API_URL, wish)
