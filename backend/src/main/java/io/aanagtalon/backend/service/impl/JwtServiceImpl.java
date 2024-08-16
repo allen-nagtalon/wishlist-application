69492a43b24feaeed18fd2dbe8431dd79c525a4a
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-08-16 12:27:43 -0700

    Implement JwtService helper methods for authentication

--- backend/src/main/java/io/aanagtalon/backend/service/impl/JwtServiceImpl.java
+++ backend/src/main/java/io/aanagtalon/backend/service/impl/JwtServiceImpl.java
@@ -4,6 +4,7 @@ import io.aanagtalon.backend.domain.Token;
 import io.aanagtalon.backend.domain.TokenData;
 import io.aanagtalon.backend.dto.User;
 import io.aanagtalon.backend.enumeration.TokenType;
+import io.aanagtalon.backend.function.TriConsumer;
 import io.aanagtalon.backend.security.JwtConfiguration;
 import io.aanagtalon.backend.service.JwtService;
 import io.aanagtalon.backend.service.UserService;
@@ -29,6 +30,9 @@ import java.util.function.Supplier;
 
 import static io.aanagtalon.backend.constant.Constants.*;
 import static io.aanagtalon.backend.enumeration.TokenType.ACCESS;
+import org.springframework.boot.web.server.Cookie.SameSite;
+
+import static io.aanagtalon.backend.enumeration.TokenType.REFRESH;
 import static java.util.Arrays.stream;
 import static java.util.Optional.empty;
 import static org.springframework.security.core.authority.AuthorityUtils.commaSeparatedStringToAuthorityList;
@@ -41,7 +45,7 @@ public class JwtServiceImpl extends JwtConfiguration implements JwtService {
 
     private final Supplier<SecretKey> key = () -> Keys.hmacShaKeyFor(Decoders.BASE64.decode(getSecret()));
 
-    private final Function<String, Claims> claimFunction = token ->
+    private final Function<String, Claims> claimsFunction = token ->
             Jwts.parser()
                     .verifyWith(key.get())
                     .build()
@@ -74,35 +78,79 @@ public class JwtServiceImpl extends JwtConfiguration implements JwtService {
                     .signWith(key.get(), Jwts.SIG.HS512);
 
     private final BiFunction<User, TokenType, String> buildToken = (user, type) ->
-            Objects.equals(type, ACCESS) ? builder.get()
-                    .subject(user.getUserId())
-                    .expiration(Date.from(Instant.now().plusSeconds(getExpiration())))
-                    .compact() : builder.get()
+            builder.get()
                     .subject(user.getUserId())
                     .expiration(Date.from(Instant.now().plusSeconds(getExpiration())))
                     .compact();
 
+    private final TriConsumer<HttpServletResponse, User, TokenType> addCookie = (response, user, type) -> {
+        switch(type) {
+            case ACCESS -> {
+                var accessToken = createToken(user, Token::getAccess);
+                var cookie = new Cookie(type.getValue(), accessToken);
+                cookie.setHttpOnly(true);
+                //cookie.setSecure(true);
+                cookie.setMaxAge(2 * 60);
+                cookie.setPath("/");
+                cookie.setAttribute("SameSite", SameSite.NONE.name());
+                response.addCookie(cookie);
+            }
+            case REFRESH -> {
+                var refreshToken = createToken(user, Token::getRefresh);
+                var cookie = new Cookie(type.getValue(), refreshToken);
+                cookie.setHttpOnly(true);
+                //cookie.setSecure(true);
+                cookie.setMaxAge(2 * 60 * 60);
+                cookie.setPath("/");
+                cookie.setAttribute("SameSite", SameSite.NONE.name());
+                response.addCookie(cookie);
+            }
+        }
+    };
+
+    private <T> T getClaimsValue(String token, Function<Claims, T> claims) {
+        return claimsFunction.andThen(claims).apply(token);
+    }
+
     public Function<String, List<GrantedAuthority>> authorities = token ->
             commaSeparatedStringToAuthorityList(new StringJoiner(AUTHORITY_DELIMITER)
-                    .add(claimFunction.apply(token).get(AUTHORITIES, String.class)).toString());
+                    .add(claimsFunction.apply(token).get(AUTHORITIES, String.class)).toString());
 
     @Override
     public String createToken(User user, Function<Token, String> tokenFunction) {
-        return "";
+        var token = Token.builder().access(buildToken.apply(user, ACCESS)).refresh(buildToken.apply(user, REFRESH)).build();
+        return tokenFunction.apply(token);
     }
 
     @Override
-    public Optional<String> extractToken(HttpServletRequest request, String tokenType) {
-        return empty();
+    public Optional<String> extractToken(HttpServletRequest request, String cookieName) {
+        return extractToken.apply(request, cookieName);
     }
 
     @Override
     public void addCookie(HttpServletResponse response, User user, TokenType type) {
-
+        addCookie.accept(response, user, type);
     }
 
     @Override
     public <T> T getTokenData(String token, Function<TokenData, T> tokenFunction) {
-        return null;
+        return tokenFunction.apply(
+                TokenData.builder()
+                        .valid(Objects.equals(userService.getUserByUserId(subject.apply(token)).getUserId(), claimsFunction.apply(token).getSubject()))
+                        .authorities(authorities.apply(token))
+                        .claims(claimsFunction.apply(token))
+                        .user(userService.getUserByUserId(subject.apply(token)))
+                        .build()
+        );
+    }
+
+    @Override
+    public void removeCookie(HttpServletRequest request, HttpServletResponse response, String cookieName) {
+        var optionalCookie = extractCookie.apply(request, cookieName);
+        if(optionalCookie.isPresent()) {
+            var cookie = optionalCookie.get();
+            cookie.setMaxAge(0);
+            response.addCookie(cookie);
+        }
     }
 }
