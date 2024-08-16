ead3391fb6f2206f35319408e03d9cc929530130
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-08-16 12:28:00 -0700

    Implement successful authentication logic and response

--- backend/src/main/java/io/aanagtalon/backend/security/AuthenticationFilter.java
+++ backend/src/main/java/io/aanagtalon/backend/security/AuthenticationFilter.java
@@ -2,7 +2,9 @@ package io.aanagtalon.backend.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.aanagtalon.backend.dto.LoginRequest;
+import io.aanagtalon.backend.dto.User;
 import io.aanagtalon.backend.enumeration.LoginType;
+import io.aanagtalon.backend.enumeration.TokenType;
 import io.aanagtalon.backend.service.JwtService;
 import io.aanagtalon.backend.service.UserService;
 import jakarta.servlet.FilterChain;
@@ -10,6 +12,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -17,9 +21,11 @@ import org.springframework.security.web.authentication.AbstractAuthenticationPro
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static com.fasterxml.jackson.core.JsonGenerator.Feature.AUTO_CLOSE_JSON_CONTENT;
 import static io.aanagtalon.backend.domain.WishlistAuthentication.unauthenticated;
+import static io.aanagtalon.backend.utils.RequestUtils.getResponse;
 import static io.aanagtalon.backend.utils.RequestUtils.handleErrorResponse;
 import static javax.swing.text.html.FormSubmitEvent.MethodType.POST;
 
@@ -50,6 +56,20 @@ public class AuthenticationFilter extends AbstractAuthenticationProcessingFilter
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        var user = (User) authResult.getPrincipal();
+        userService.updateLoginAttempt(user.getEmail(), LoginType.LOGIN_SUCCESS);
+        var httpResponse = sendResponse(request, response, user);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.OK.value());
+        var output = response.getOutputStream();
+        var mapper = new ObjectMapper();
+        mapper.writeValue(output, httpResponse);
+        output.flush();
+    }
+
+    private Object sendResponse(HttpServletRequest request, HttpServletResponse response, User user) {
+        jwtService.addCookie(response, user, TokenType.ACCESS);
+        jwtService.addCookie(response, user, TokenType.REFRESH);
+        return getResponse(request, Map.of("user", user), "Login Success", HttpStatus.OK);
     }
 }
