bc4e4b946a5d220da84d96ec761d3ac68ca72f3d
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-08-23 23:15:18 -0700

    Change the background image for Landing page

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,5 +1,5 @@
-import { Box, Button, Typography } from "@mui/material"
-import bg from "../assets/landing-bg.jpg"
+import { Box, Button, Typography } from '@mui/material'
+import bg from '../assets/landing-bg.jpg'
 
 function LandingPage () {
   return (
