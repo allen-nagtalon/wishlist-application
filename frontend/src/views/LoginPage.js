0f80c89ca102185751512464908498793bf78a05
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-08-24 02:58:53 -0700

    Add texture to the page by putting sign in form on Paper component

--- frontend/src/views/LoginPage.js
+++ frontend/src/views/LoginPage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Container, FormControlLabel, Grid, TextField, Typography } from '@mui/material'
+import { Box, Button, Checkbox, Container, Divider, FormControlLabel, Grid, Paper, TextField, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 
 function LoginPage () {
@@ -10,64 +10,71 @@ function LoginPage () {
     <Container maxWidth='sm'>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
           alignItems: 'center',
-          mt: 10
+          mt: 18
         }}
       >
-        <Typography component='h2' variant='h5' sx={{ fontWeight: 600 }}>
-          Sign In
-        </Typography>
-        <Box component='form' onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <TextField
-            required
-            fullWidth
-            id='username'
-            label='Username/Email'
-            name='username'
-            autoFocus
-          />
-          <TextField
-            required
-            fullWidth
-            id='password'
-            label='Password'
-            type='password'
-            name='password'
-            autoFocus
-            sx={{ my: 2}}
-          />
-          <FormControlLabel
-            control={<Checkbox value='remember' color='primary' />}
-            label='Remember me'
-          />
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{
-              mt: 3,
-              mb: 2,
-              bgcolor: 'accent.main',
-              color: 'text.light'
-            }}
-          >
+        <Paper
+          elevation={2}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            p: 5
+          }}
+        >
+          <Typography component='h2' variant='h5' sx={{ fontWeight: 600 }}>
             Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item xs>
-              <Link href='#' variant='body2'>
-                Don't have an account? Sign Up
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
+          </Typography>
+          <Box component='form' onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <TextField
+              required
+              fullWidth
+              id='username'
+              label='Username/Email'
+              name='username'
+              autoFocus
+            />
+            <TextField
+              required
+              fullWidth
+              id='password'
+              label='Password'
+              type='password'
+              name='password'
+              autoFocus
+              sx={{ my: 2 }}
+            />
+            <FormControlLabel
+              control={<Checkbox value='remember' color='primary' />}
+              label='Remember me'
+            />
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{
+                mt: 3,
+                mb: 2,
+                bgcolor: 'accent.main',
+                color: 'text.light'
+              }}
+            >
+              Sign In
+            </Button>
+          </Box>
+          <Typography variant='body2' sx={{ my: 2 }}>
+            <Link href='#'>
+              Forgot password?
+            </Link>
+          </Typography>
+          <Divider variant='middle' flexItem>or</Divider>
+          <Typography variant='body2' sx={{ my: 2 }}>
+            <Link href='#'>
+              Don't have an account? Sign Up
+            </Link>
+          </Typography>
+        </Paper>
       </Box>
     </Container>
   )
