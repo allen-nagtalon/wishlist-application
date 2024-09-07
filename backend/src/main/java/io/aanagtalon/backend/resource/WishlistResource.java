0f905e4dfaf9e318f602da3927b5578df7f07d7e
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-09-06 23:14:18 -0700

    Implement API endpoint to create a new wishlist

--- backend/src/main/java/io/aanagtalon/backend/resource/WishlistResource.java
+++ backend/src/main/java/io/aanagtalon/backend/resource/WishlistResource.java
@@ -1,17 +1,21 @@
 package io.aanagtalon.backend.resource;
 
+import io.aanagtalon.backend.domain.Response;
 import io.aanagtalon.backend.dto.WishlistRequest;
-import io.aanagtalon.backend.entity.WishlistEntity;
 import io.aanagtalon.backend.service.JwtService;
 import io.aanagtalon.backend.service.WishlistService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Map;
+
+import static io.aanagtalon.backend.utils.RequestUtils.getResponse;
+import static java.util.Collections.emptyMap;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/wishlist")
@@ -20,12 +24,13 @@ public class WishlistResource {
     private final WishlistService wishlistService;
     private final JwtService jwtService;
 
-    @PostMapping("/create")
-    public ResponseEntity<WishlistEntity> createWishlist(@RequestBody WishlistRequest request) {
-
+    @PostMapping()
+    public ResponseEntity<Response> createWishlist(@RequestHeader(name = "Authorization") String token, @RequestBody WishlistRequest wishlist, HttpServletRequest request) {
+        var ownerId = jwtService.extractUserId(token.substring(4));
+        wishlistService.createWishlist(wishlist.getTitle(), wishlist.getDescription(), ownerId);
         return ResponseEntity
                 .created(URI.create("/wishlist/id"))
-                .body(wishlistService.createWishlist(request.getTitle(), request.getDescription(), request.getOwnerId()));
+                .body(getResponse(request, emptyMap(), "Wishlist created.", CREATED));
     }
 
 }
--- backend/src/main/java/io/aanagtalon/backend/service/WishlistService.java
+++ backend/src/main/java/io/aanagtalon/backend/service/WishlistService.java
@@ -3,5 +3,5 @@ package io.aanagtalon.backend.service;
 import io.aanagtalon.backend.entity.WishlistEntity;
 
 public interface WishlistService {
-    public WishlistEntity createWishlist(String title, String description, Long ownerId);
+    WishlistEntity createWishlist(String title, String description, Long ownerId);
 }
