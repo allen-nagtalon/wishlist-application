b2c8d64859cfb1e368f5ffbda3e1dbf89200ddd6
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2024-09-10 13:58:09 -0700

    Create a basic web page for displaying the wishes of a given wishlist

---
+++ frontend/src/components/WishCardList/WishCardList.js
@@ -0,0 +1,61 @@
+import { Box, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
+import EditIcon from '@mui/icons-material/Edit'
+import LinkIcon from '@mui/icons-material/Link'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+function WishCardList (props) {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          {
+            props.wishes.map((wish, i) => (
+              <Grid key={i} xs={4}>
+                <Card sx={{ borderRadius: 3 }}>
+                  <CardMedia
+                    component='img'
+                    image={wish.photoUrl}
+                    sx={{
+                      height: '200px'
+                    }}
+                  />
+                  <CardContent>
+                    <Typography sx={{ fontWeight: 600 }}>
+                      {wish.title}
+                    </Typography>
+                    <Typography
+                      variant='body2'
+                      sx={{
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        WebkitLineClamp: '2',
+                        WebkitBoxOrient: 'vertical'
+                      }}
+                    >
+                      {wish.desc}
+                    </Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <IconButton>
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton>
+                      <LinkIcon />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))
+          }
+        </Grid>
+      </Box>
+    </>
+  )
+}
+
+export default WishCardList
--- frontend/src/views/WishListView.js
+++ frontend/src/views/WishListView.js
@@ -1,68 +1,65 @@
-import { Box, Button, Card, CardContent, CardMedia, Container, IconButton, Typography } from '@mui/material'
-import LinkIcon from '@mui/icons-material/Link'
-import EditIcon from '@mui/icons-material/Edit'
+import { Box, Button, Container, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import { useEffect, useState } from 'react'
+import WishCardList from '../components/WishCardList/WishCardList'
+import { useParams } from 'react-router-dom'
 
-const wishes = [
+const testWishes = [
   {
     title: 'XG OFFICIAL LIGHT STICK Ver.1（w/XG Trading Card / Set of 7)',
     desc: 'Finally, the official XG light stick is now available! With a design reminiscent of a spaceship and featuring a trigger-type button, it can switch between seven member colours and auto colour change in normal performance mode.',
     url: 'https://shop.xgalx.com/collections/all-item/products/xglx-0100',
-    image: 'https://shop.xgalx.com/cdn/shop/files/XGLX-0100_t_01.jpg?v=1714458221'
+    photoUrl: 'https://shop.xgalx.com/cdn/shop/files/XGLX-0100_t_01.jpg?v=1714458221'
   },
   {
-    title: 'XG OFFICIAL LIGHT STICK Ver.1（w/XG Trading Card / Set of 7)',
-    desc: 'Finally, the official XG light stick is now available! With a design reminiscent of a spaceship and featuring a trigger-type button, it can switch between seven member colours and auto colour change in normal performance mode.',
-    url: 'https://shop.xgalx.com/collections/all-item/products/xglx-0100',
-    image: 'https://shop.xgalx.com/cdn/shop/files/XGLX-0100_t_01.jpg?v=1714458221'
+    title: 'The Legend of Zelda™: Echoes of Wisdom',
+    desc: 'The Legend of Zelda: Echoes of Wisdom is an upcoming video game in The Legend of Zelda series published by Nintendo for the Nintendo Switch.',
+    url: 'https://www.nintendo.com/us/store/products/the-legend-of-zelda-echoes-of-wisdom-119253/',
+    photoUrl: 'https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70010000084618/e3a49cf14180cec09c515595fb7121b048a0f8327eae2d248909d0f0c64a9dc9'
+  },
+  {
+    title: 'Apari Bunny Twins Skate Deck',
+    desc: 'Skate deck from artist @ApariArt.',
+    url: 'https://www.apari-shop.com/products/bunny-twins-skate-deck',
+    photoUrl: 'https://www.apari-shop.com/cdn/shop/files/bunnycloseup.jpg?v=1684531571&width=990'
+  },
+  {
+    title: 'Uniqlo Mini Shoulder Bag',
+    desc: 'Color: 09 BLACK',
+    url: 'https://www.uniqlo.com/us/en/products/E472032-000/00?colorDisplayCode=09',
+    photoUrl: 'https://image.uniqlo.com/UQ/ST3/WesternCommon/imagesgoods/472032/sub/goods_472032_sub13_3x4.jpg'
   }
 ]
 
-function WishListView () {
+function WishListView (props) {
+  const { wishlistId } = useParams()
+  const [wishes, setWishes] = useState(null)
+
+  const fetchWishes = () => {
+    console.log(`Fetching wishes for wishlist ${wishlistId} from API`)
+  }
+
+  useEffect(() => {
+    fetchWishes()
+  }, [])
+
   return (
-    <Container maxWidth='lg'>
-      <Box>
-        <Typography variant='h2'>
-          Jurin's Wishlist
-        </Typography>
-        <Button>
-          Add Wish
+    <Container maxWidth='lg' sx={{ pt: 10, flexGrow: 1 }}>
+      <Typography variant='h4' sx={{ mb: 3 }}>
+        Jurin's Wishlist
+      </Typography>
+      <Box sx={{ mb: 3 }}>
+        <Button
+          component='label'
+          variant='contained'
+          startIcon={<AddIcon />}
+          color='light'
+          sx={{ borderRadius: 5 }}
+        >
+          Add New Wish
         </Button>
-        {
-          wishes.map((wish) =>
-            <Card
-              key={wish.title}
-              sx={{
-                display: 'flex',
-                px: 5,
-                py: 5,
-                my: 5
-              }}
-            >
-              <CardMedia
-                component='img'
-                sx={{ width: 200 }}
-                image={wish.image}
-                alt='A wish picture'
-              />
-              <CardContent>
-                <Box>
-                  <Typography variant='h5'>
-                    {wish.title}
-                  </Typography>
-                </Box>
-                <Box sx={{ display: 'flex', verticalAlign: 'middle' }}>
-                  <IconButton>
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton component='a' href={wish.url}>
-                    <LinkIcon />
-                  </IconButton>
-                </Box>
-              </CardContent>
-            </Card>
-          )
-        }
       </Box>
+      <WishCardList wishes={testWishes} />
     </Container>
   )
 }
